cd28ba05e249466f0b63595bf717d99d875dbfbc
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-11-10 12:30:17 +0000

    Added breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title('Test Application')
+streamlit.title('Test Application Menu')
+streamlit.header('Breakfast')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Bacon panckaes')
+streamlit.text('Full English')
