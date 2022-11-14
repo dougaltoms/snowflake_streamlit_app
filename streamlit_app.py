1d79c09fc52f3a3434000d3c8dd4119ba10647ad
Dougal Toms <104922765+dougaltoms@users.noreply.github.com>
2022-11-14 17:55:37 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ st.dataframe(to_display)
 st.header('FruityVice Fruit Advice')
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('You have entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/", fruit_choice)
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
